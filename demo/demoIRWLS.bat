1bf7600472bcc79c186b22b539d096bf2168478a
RobeDM <rober.diaz@gmail.com>
2016-09-25 04:23:48 +0200

    New demo scripts

--- demo/demoIRWLS.bat
+++ demo/demoIRWLS.bat
@@ -1,6 +1,21 @@
 :: Test de la libreria
  
 @echo off 
+
+ECHO ***************************************************
+ECHO * DOWNLOADING DATASETS FROM THE LIBSVM REPOSITORY *
+ECHO ***************************************************
+ECHO 
+ 
+mkdir ..\data 
+ 
+ECHO Downloading ADULT dataset for training: a9a
+
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/a9a', '..\data\a9a')" 
+
+ECHO Downloading ADULT dataset for testing: a9a.t
+ 
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/a9a.t', '..\data\a9a.t')" 
  
 If exist "..\windows\PIRWLS-train.exe" (
 	
@@ -9,14 +24,14 @@ ECHO * RUNNING SVM ^(PIRWLS algorithm^) USING 1 THREAD *
 ECHO *************************************************
 ECHO  
 
-..\windows\PIRWLS-train.exe -c 100 -g 0.1 -t 1 ..\data\w7a ..\data\w7a.model		
+..\windows\PIRWLS-train.exe -c 1000 -g 0.001 -t 1 ..\data\a9a ..\data\a9a.model		
 
 ECHO **************************************************
 ECHO * RUNNING SVM ^(PIRWLS algorithm^) USING 2 THREADS *
 ECHO **************************************************
 ECHO  
 
-..\windows\PIRWLS-train.exe -c 100 -g 0.1 -t 2 ..\data\w7a ..\data\w7a.model			
+..\windows\PIRWLS-train.exe -c 1000 -g 0.001 -t 2 ..\data\a9a ..\data\a9a.model			
 	
 
 ECHO *****************************************************
@@ -24,10 +39,10 @@ ECHO * USING THE MODEL CREATED TO CLASSIFY A NEW DATASET *
 ECHO *****************************************************
 ECHO  
 
-..\windows\LIBIRWLS-predict.exe -l 1 -t 1 ..\data\w7a.t ..\data\w7a.model ..\data\w7a.output	
+..\windows\LIBIRWLS-predict.exe -l 1 -t 1 ..\data\a9a.t ..\data\a9a.model ..\data\a9a.output	
 	
 ) ELSE (
-ECHO ..\bin\PIRWLS-train.exe  not found. Please, build this code using the make command.
+ECHO ..\bin\PIRWLS-train.exe not found.
 )
 
 
@@ -38,23 +53,23 @@ ECHO ******************************************************
 ECHO * RUNNING SEMIPARAMETRIC SVM ^(PSIRWLS^) USING 1 THREAD *
 ECHO *******************************************************
 ECHO  
-..\windows\PSIRWLS-train.exe -c 100 -s 100 -g 0.1 -t 1 ..\data\w7a ..\data\w7a.model		
+..\windows\PSIRWLS-train.exe -c 1000 -s 75 -g 0.0001 -t 1 ..\data\a9a ..\data\a9a.model		
 
 ECHO ********************************************************
 ECHO * RUNNING SEMIPARAMETRIC SVM ^(PSIRWLS^) USING 2 THREADS *
 ECHO ********************************************************
 
 ECHO    
-..\windows\PSIRWLS-train.exe -c 100 -s 100 -g 0.1 -t 2 ..\data\w7a ..\data\w7a.model			
+..\windows\PSIRWLS-train.exe -c 1000 -s 75 -g 0.0001 -t 2 ..\data\a9a ..\data\a9a.model			
 	
 ECHO *****************************************************
 ECHO * USING THE MODEL CREATED TO CLASSIFY A NEW DATASET *
 ECHO *****************************************************
 ECHO  
 
-..\windows\LIBIRWLS-predict.exe -l 1 -t 1 ..\data\w7a.t ..\data\w7a.model ..\data\w7a.output	
+..\windows\LIBIRWLS-predict.exe -l 1 -t 1 ..\data\a9a.t ..\data\a9a.model ..\data\a9a.output	
 	
 	
 ) ELSE (
-ECHO ..\bin\PIRWLS-train.exe  not found. Please, build this code using the make command.
+ECHO ..\bin\PIRWLS-train.exe not found.
 )
