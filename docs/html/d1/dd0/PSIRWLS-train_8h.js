61e1d1326e1144fe6a9ec9126456a1d9b29727dd
RobeDM <rober.diaz@gmail.com>
2017-07-22 11:31:26 +0200

    New command line names

--- docs/html/d1/dd0/PSIRWLS-train_8h.js
+++
@@ -1,9 +0,0 @@
-var PSIRWLS_train_8h =
-[
-    [ "calculatePSIRWLSModel", "d1/dd0/PSIRWLS-train_8h.html#a71b4329438bbf3210414315619f7b804", null ],
-    [ "IRWLSpar", "d1/dd0/PSIRWLS-train_8h.html#ad51d9a46645ad0b0bedb1113a3807d24", null ],
-    [ "parseTrainParameters", "d1/dd0/PSIRWLS-train_8h.html#a67566f6fd6aec7278ca360186af4e91b", null ],
-    [ "printPSIRWLSInstructions", "d1/dd0/PSIRWLS-train_8h.html#a0a24887780df491bc4ea2f590a2198f7", null ],
-    [ "randomCentroids", "d1/dd0/PSIRWLS-train_8h.html#aa6c8aa8b86b7a0647cc679408c310cd3", null ],
-    [ "SGMA", "d1/dd0/PSIRWLS-train_8h.html#a50a6e1f36a010feb91db9b9b38679948", null ]
-];
